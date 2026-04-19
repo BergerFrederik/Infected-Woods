71b4d8e9617dd144e67d99f224cb695e47017aa9
Frederik Berger <freddi.berger@gmx.de>
2026-04-19 22:35:42 +0200

    Start weapon LVL system

---
+++ Assets/Scripts/Weapons/WeaponData.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewWeaponData", menuName = "ScriptableObjects/WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    [Header("Allgemeine Infos")]
+    public string weaponName;
+    public string weaponSubtitle;
+    public float weaponPrice;
+    public int weaponTier; 
+
+    [Header("Level-Spezifische Stats")]
+    public LevelStats[] levels; 
+
+    [System.Serializable]
+    public class LevelStats
+    {
+        public string weaponName;
+        public string weaponSubtitle;
+        public float weaponProjectileSpeed = 0f;
+        public float weaponBaseDamage = 0f;
+        public float weaponMeeleDamageScale = 0f;
+        public float weaponRangedDamageScale = 0f;
+        public float weaponMysticDamageScale = 0f;
+        public float weaponTier = 0f;
+        public float weaponLevel;
+    
+        public enum weaponTypeOptions
+        {
+            Melee,
+            Ranged,
+            Ability
+        }
+        public weaponTypeOptions weaponWeaponType;
+        public string weaponAttackType;
+        public float weaponClass = 0f;
+        public float weaponAttackSpeedCooldown = 0f; // in seconds
+        public float weaponCritChance = 0f;
+        public float weaponCritDamage = 0f;
+        public float weaponRange = 0f;
+        public float weaponKnockback = 0f;
+        public float weaponLifesteal = 0f;
+        public float weaponPrice = 0f;
+        public float currentTotalDamage = 0f;
+        public string weaponSpecialAbility;
+        public string weaponLore;
+    }
+}
