b9ca569b75a3d2d160cec2c9e2b3b368ff1f1b11
BergerFrederik <frederik.berger@exxeta.com>
2026-08-03 09:15:47 +0200

    variable name changes

--- Assets/Scripts/Weapons/Weapons/SpiritBow.cs
+++ Assets/Scripts/Weapons/Weapons/SpiritBow.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class SpiritBow : MonoBehaviour
 {
-    [SerializeField] private float _chanceToGainAttackspeed;
-    [SerializeField] private float _attackspeedBoost;
-    [SerializeField] private float _attackspeedBoostTime;
-    [SerializeField] private Ranged _ranged;
+    [SerializeField] private float chanceToGainAttackspeed;
+    [SerializeField] private float attackspeedBoost;
+    [SerializeField] private float attackspeedBoostTime;
+    [SerializeField] private Ranged ranged;
     
     private GameObject _playerObject;
     private PlayerStats _playerStats;
@@ -17,18 +17,18 @@ public class SpiritBow : MonoBehaviour
         _playerObject = GameObject.FindGameObjectWithTag("Player");
         _playerStats = _playerObject.GetComponent<PlayerStats>();
         _randomRollEvent = _playerObject.GetComponentInChildren<RandomRollEvent>();
-       _ranged.OnWeaponProjectileHitsEnemy += IncreaseAttackSpeed;
+       ranged.OnWeaponProjectileHitsEnemy += IncreaseAttackSpeed;
     }
 
     private void OnDestroy()
     {
-        _ranged.OnWeaponProjectileHitsEnemy -= IncreaseAttackSpeed;
+        ranged.OnWeaponProjectileHitsEnemy -= IncreaseAttackSpeed;
     }
 
     private void IncreaseAttackSpeed()
     {
         float rndNum = _randomRollEvent.GetRandomFloatRoll(0f, 100f);
-        if (rndNum < 1 - _chanceToGainAttackspeed) //Muss 1- sein, damit luck einen Einfluss hat. Luck erhöht den Roll
+        if (rndNum > 100f - chanceToGainAttackspeed) //Muss 100- sein, damit luck einen Einfluss hat. Luck erhöht den Roll
         {
             StartCoroutine(GainAttackSpeedForSeconds());
         }
@@ -36,8 +36,8 @@ public class SpiritBow : MonoBehaviour
 
     private IEnumerator GainAttackSpeedForSeconds()
     {
-        _playerStats.playerAttackSpeed += _attackspeedBoost;
-        yield return new WaitForSeconds(_attackspeedBoostTime);
-        _playerStats.playerAttackSpeed -= _attackspeedBoost;
+        _playerStats.playerAttackSpeed += attackspeedBoost;
+        yield return new WaitForSeconds(attackspeedBoostTime);
+        _playerStats.playerAttackSpeed -= attackspeedBoost;
     }
 }
