2c0b3b4a048881a90fea46da7bc384cd47566342
BergerFrederik <frederik.berger@exxeta.com>
2026-08-11 23:35:55 +0200

    start of new weapon Sai

---
+++ Assets/Scripts/Weapons/Weapons/Melee/Shroom/Sai.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Sai : MonoBehaviour
+{
+    [SerializeField] private PlayerStats playerStats;
+    [SerializeField] private RandomRollEvent randomRollEvent;
+    
+    
+}
